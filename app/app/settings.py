1db8ad687d2d30bff670cd219db1435794c439ba
shivbagdi3 <shivbagdi3@gmail.com>
2024-05-09 19:08:12 +0530

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spactacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
